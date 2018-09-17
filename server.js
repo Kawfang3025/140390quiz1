d4df56ba293bc09c09b6cc585a93aa8fe19c63d8
Kawfang3025 <5930213025@email.psu.ac.th>
2018-09-17 16:01:14 +0700

    install npm all

--- server.js
+++ server.js
@@ -1,17 +1,4 @@
-var express = require('express');
-var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 
-var app = express();
-// app.get('/',function(request, response){
-// response.send('<H1>Hello, ExpressJS 555</H1>');
-// });
-
-// app.get('/test',function(request, response){
-//     response.send('<H1>Test</H1>');
-// });
-
-// app.use(express.static('static'));
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
     res.render('pages/index');
@@ -23,19 +10,6 @@ app.get('/about', function (req, res) {
     var bdate = '06/01/1998';
     res.render('pages/about', { fullname: name, hobbies: hobbies, bdate: bdate });
 });
-
-// app.get('/products', function (req, res) {
-//     db.any('select* from products,')
-//         .then(function (data) {
-//             console.log('DATA:' + data)
-//             res.render('page/products',{products :data});
-//         })
-//         .catch(function (error) {
-//             console.log('ERROR:' + error)
-//         })
-//     });
-
-
 app.get('/products', function (req, res) {
     var id = req.param('id');
     var sql = 'select * from products';
@@ -51,9 +25,7 @@ app.get('/products', function (req, res) {
             .catch(function (error) {
                 console.log('ERROR:' + error);
             })
-
 });
-
 app.get('/users', function (req, res) {
     db.any('select * from users')
         .then(function (data) {
@@ -64,7 +36,6 @@ app.get('/users', function (req, res) {
             console.log('ERROR:' + error);
         })
 });
-
 app.get('/users/:id', function (req, res) {
     var id = req.params.id;
     var sql = 'select * from users';
@@ -83,11 +54,8 @@ app.get('/users/:id', function (req, res) {
         })
 });
 
-
 app.get('/index', function (req, res) {
     res.render('pages/index');
 });
 console.log('App is running at http://localhost:8080');
-
-
 app.listen(8080);
