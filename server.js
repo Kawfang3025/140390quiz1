927130c39f4a65ec9bad213faeedc9395c540671
Kawfang3025 <5930213025@email.psu.ac.th>
2018-09-17 17:12:53 +0700

    can use subjects module

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var app = express();
 var mysql = require('mysql')
 var connection = mysql.createConnection({
   host     : 'www.db4free.net',
@@ -7,23 +7,12 @@ var connection = mysql.createConnection({
   password : 'abc123**',
   database : 'db140390'
 });
-
-
-var app = express();
-
 app.set('view engine', 'ejs');
+//home
 app.get('/', function (req, res) {
     res.render('pages/index');
 });
-
-app.get('/about', function (req, res) {
-    var name = 'CHAWEEWAN SOOKWAN';
-    var hobbies = ['Music', 'Movie', 'Programming'];
-    var bdate = '06/01/1998';
-    res.render('pages/about', { fullname: name, hobbies: hobbies, bdate: bdate });
-});
-
-
+//all list of student
 app.get('/students', function (req, res) {
     connection.connect()
     var sql ='select * from students';
@@ -39,12 +28,22 @@ connection.end()
 
 });
 
+//all list of subjects
+app.get('/subjects', function (req, res) {
+    connection.connect()
+    var sql ='select * from subjects';
+    connection.query(sql,function (err, rows, fields){
+        if (err) throw err
+        res.render('pages/subjects',{ subjects:rows});
+    console.log('The subject is: ', rows[0].subjects)
+})
+connection.end()
 
+});
 
 app.get('/index', function (req, res) {
     res.render('pages/index');
 });
 console.log('App is running at http://localhost:8081');
-
 
 app.listen(8081);
